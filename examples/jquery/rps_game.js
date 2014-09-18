9ee696b42120b53fa6bf3bfcc0077ebba7d2e9f3
[cdonohue1] <[chasedonohue01@gmail.com]>
2014-09-18 09:02:05 -0600

    created rock papper scissors game with js, html and css

---
+++ examples/jquery/rps_game.js
@@ -0,0 +1,78 @@
+$(document).ready(function(){
+     var choices =['rock', 'paper', 'scissors'];
+     var userChoice = '';
+     var wins = 0;
+     var loses = 0;
+     var cpuChoice = Math.random();
+     console.log(cpuChoice);
+        if (cpuChoice < 0.34) {
+            cpuChoice = "rock";
+        } else if(cpuChoice <= 0.67) {
+            cpuChoice = "paper";
+        } else {
+            cpuChoice = "scissors";
+        }
+
+    // $('.choice').select("userChoice(this)"){
+    // }
+    // function userChoice(choice) {
+    // }
+    $('.choice').change(function() {
+      userChoice = $(this).val();
+      result(userChoice, cpuChoice);
+    });
+    // console.log("TEST");
+    // $('.cpuChoice')document.console.log = cpuChoice
+
+    //  $('.choice')(function(){ //Confused
+    //   if('.choice' == 'rock'){
+    //     userChoice == 'rock'
+    //   };
+
+
+    //  });
+    //  $('.agian').click(function){
+
+    //  });
+
+
+     function result(userChoice, cpuChoice){
+      var result = '';
+      console.log(userChoice);
+      console.log(cpuChoice);
+      if(userChoice == cpuChoice) { //why not three ====? 
+        result = 'Tie'; 
+        } 
+      else if(userChoice == 'rock'){
+
+        if (cpuChoice == 'paper'){
+          result = 'Lose'; //has only one =
+          loses++;
+         } else {
+            result = 'Win'; //scissors
+            wins++;
+         }; 
+      }
+      else if(userChoice =='paper'){
+
+        if (cpuChoice == 'scissors'){
+          result = 'Lose';
+          loses++; 
+        } else{
+          result = 'Win';
+          wins++;
+        };
+      }
+      else if(userChoice == 'scissors'){
+        if (cpuChoice == 'rock'){
+          result = 'Lose';
+          loses++;
+        } else{
+          result = 'Win';
+          wins++;
+        };
+      };
+      // return result;
+      alert("what hapened:" + result); 
+     };
+  });
